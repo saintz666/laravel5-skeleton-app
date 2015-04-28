a4537d1758e527d0bb8662d87ed0e55286b3b214
James Gordon <jamesg@cryptopia.com.au>
2015-04-28 21:04:16 +1000

    Small mod to create class alias for Form handler

--- config/app.php
+++ config/app.php
@@ -198,6 +198,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+        'Form'      => 'Illuminate\Html\FormFacade',
+        'Html'      => 'Illuminate\Html\HtmlFacade',
 
 	],
 
